b19682cc26dc444e5fb024bf88de32f18898b8cf
Paula N Guedes <paulanguedes@gmail.com>
2020-08-24 18:42:14 -0300

    add contact page

--- resources/views/AddContact.blade.php
+++ resources/views/AddContact.blade.php
@@ -1,9 +1,46 @@
-@extends('template')
+@extends('navbar')
 
-@section('title')
-    Novo contato
-@endsection
+@section('title') Novo contato @endsection
+
+<link rel="stylesheet" href="../css/add-contact.css">
 
 @section('main')
-    <h2>Novo contato</h2>
+    
+    <main>
+        <h2>Novo contato</h2>
+        <h5>*Todos os campos são obrigatórios</h5>
+
+        <form method="post">
+
+            <div id="customer">
+                <label for="customer">Cliente</label>
+                <br>
+                <select name="customer" id="customer" required>
+                    <option disabled  value="">Escolha uma opção</option>
+                    <option value="cliente2">Cliente 2</option>
+                    <option value="cliente3">Cliente 3</option>
+                    <option value="cliente4">Cliente 4</option>
+                </select>
+            </div>
+            <div id="name">
+                <label for="name">Nome completo da empresa</label>
+                <br>
+                <input type="text" name="name" id="name" required>
+            </div>
+            <div id="email">
+                <label for="email">E-mails (separe os e-mails com vírgula)</label>
+                <br>
+                <input type="email" name="email" id="email" multiple required>
+            </div>
+            <div id="phone">
+                <label for="phone">Telefones (separe os e-mails com vírgula)</label>
+                <br>
+                <input type="tel" name="phone" id="phone" multiple pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+            </div>
+            
+            <button type="submit">enviar</button>
+
+        </form>
+    </main>
+
 @endsection
